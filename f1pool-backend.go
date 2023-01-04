3d10db7a1ee5d2ddf144814490afb77b72af8265
Juan Molano <juan.estebanmolano@swisscom.com>
2023-01-04 15:59:06 +0100

    Try to connect to private db

--- f1pool-backend.go
+++ f1pool-backend.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
+
+	"cloud.google.com/go/cloudsqlconn"
+	"github.com/go-sql-driver/mysql"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
@@ -33,6 +38,54 @@ func setupDB() *sql.DB {
 	checkErr(err)
 
 	return db
+}
+
+func connectWithConnector() (*sql.DB, error) {
+	//mustGetenv := func(k string) string {
+	//	v := os.Getenv(k)
+	//	if v == "" {
+	//		log.Fatalf("Warning: %s environment variable not set.", k)
+	//	}
+	//	return v
+	//}
+	// Note: Saving credentials in environment variables is convenient, but not
+	// secure - consider a more secure solution such as
+	// Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+	// keep secrets safe.
+	var (
+		dbUser                 = DB_USER                       // e.g. 'my-db-user'
+		dbPwd                  = DB_PASSWORD                   // e.g. 'my-db-password'
+		dbName                 = DB_NAME                       // e.g. 'my-database'
+		instanceConnectionName = "f1pools:europe-west4:f1pool" // e.g. 'project:region:instance'
+		usePrivate             = "10.115.112.3"
+		//dbUser                 = mustGetenv("DB_USER")                  // e.g. 'my-db-user'
+		//dbPwd                  = mustGetenv("DB_PASS")                  // e.g. 'my-db-password'
+		//dbName                 = mustGetenv("DB_NAME")                  // e.g. 'my-database'
+		//instanceConnectionName = mustGetenv("INSTANCE_CONNECTION_NAME") // e.g. 'project:region:instance'
+		//usePrivate             = os.Getenv("PRIVATE_IP")
+	)
+
+	d, err := cloudsqlconn.NewDialer(context.Background())
+	if err != nil {
+		return nil, fmt.Errorf("cloudsqlconn.NewDialer: %v", err)
+	}
+	var opts []cloudsqlconn.DialOption
+	if usePrivate != "" {
+		opts = append(opts, cloudsqlconn.WithPrivateIP())
+	}
+	mysql.RegisterDialContext("cloudsqlconn",
+		func(ctx context.Context, addr string) (net.Conn, error) {
+			return d.Dial(ctx, instanceConnectionName, opts...)
+		})
+
+	dbURI := fmt.Sprintf("%s:%s@cloudsqlconn(localhost:3306)/%s?parseTime=true",
+		dbUser, dbPwd, dbName)
+
+	dbPool, err := sql.Open("mysql", dbURI)
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open: %v", err)
+	}
+	return dbPool, nil
 }
 
 type Movie struct {
@@ -96,7 +149,8 @@ func checkErr(err error) {
 
 // response and request handlers
 func GetMovies(w http.ResponseWriter, r *http.Request) {
-	db := setupDB()
+	//db := setupDB()
+	db, err := connectWithConnector()
 
 	printMessage("Getting movies...")
 
